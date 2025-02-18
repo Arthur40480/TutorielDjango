d86e8d20a8e7ee929ffa8068817f19f1fc690692
Arthur Gibert <arthurgibert@free.fr>
2025-02-18 09:35:49 +0100

    2.2.3 Exercice d'écriture de méthodes du modèle [optionnel]

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_text", "pub_date")
+    list_filter = ("pub_date", "question_text")
+    search_fields = ("question_text", "pub_date")
+    ordering = ("pub_date",)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ("question", "choice_text", "votes")
+    list_filter = ("question", "votes",)
+    search_fields = ("choice_text", "votes")
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,16 +2,44 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+MAX_LENGTH = 20
+
+
+def text_excerpt(text, max_length):
+    return text[:max_length] + ('...' if len(text) > max_length else '')
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
-        return self.question_text
+        return "{} {}".format(self.pub_date,
+                              text_excerpt(self.question_text,
+                                           MAX_LENGTH))
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def age(self):
+        return timezone.now() - self.pub_date
+
+    def get_choices(self):
+        choices = self.choice_set.all()
+        total = sum(c.votes for c in choices)
+        if total == 0:
+            return [(c.choice_text, c.votes, 0) for c in choices]
+        return [(c.choice_text, c.votes, c.votes / total) for c in choices]
+
+    def get_max_choice(self):
+        choices = self.choice_set.all()
+        if not choices:
+            return None, 0
+        total = sum(c.votes for c in choices)
+        if total == 0:
+            return None, 0
+        max_choice = max(choices, key=lambda c: c.votes)
+        return max_choice.choice_text, max_choice.votes / total
 
 
 class Choice(models.Model):
@@ -20,4 +48,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return text_excerpt(self.choice_text, MAX_LENGTH)
